e006ebfe64493016bdb61491b18e62b294bf3020
shreyask0712 <shreyask0712@gmail.com>
2024-10-18 16:57:07 +0530

    sfdc Accounts - list accounts

--- services/sfdcAccounts.ts
+++ services/sfdcAccounts.ts
@@ -1,44 +1,5 @@
-import { Client, Pool } from "pg";
-import soap from "soap";
-import {config} from "dotenv";
-config();
+import { Context,query } from './sfdcService';
 
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL
-})
-
-export async function login(req, res) {
-    const { username, password } = req.body;
-    const userQuery = 'SELECT * from users WHERE username = $1 AND password = $2';
-
-    const client = await pool.connect();
-    const userResult = await client.query(userQuery, [username, password]);
-
-    if (userResult.rowCount===0) {
-        return res.status(401).send({error:'Invalid credentials'});
-    }
-
-    const wsdlUrl = './salesforce-partner.wsdl.xml';
-    const soapClient = await soap.createClientAsync(wsdlUrl);
-
-    const loginResult = await soapClient.loginAsync({
-        username: process.env.SALESFORCE_USERNAME,
-        password: `${process.env.SALESFORCE_PASSWORD}${process.env.SALESFORCE_TOKEN}`
-    })
-
-    const sessionId = loginResult.sessionId;
-    const serverUrl = loginResult.serverUrl;
-
-    return res.send({sessionId, serverUrl});
-}
-
-export async function register (req, res) {
-    const { username, password } = req.body;
-    const insertQuery = 'INSERT INTO users (username, password) VALUES ($1, $2)';
-
-    const client = await pool.connect();
-
-    await client.query(insertQuery, [username, password]);
-
-    return res.send({message: 'User registered succesfully'});
+export async function listAccounts(context: Context) {
+    return await query(context, 'SELECT Id, Name, BillingStreet, BillingCity, BillingState, BillingPostalCode, Website FROM Accounts');
 }
