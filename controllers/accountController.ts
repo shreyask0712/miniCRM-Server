547940d113f0cc5bfc559ff21f2160b34ed8d2a6
shreyask0712 <shreyask0712@gmail.com>
2024-10-18 17:39:18 +0530

    updated account controller - list accounts

--- controllers/accountController.ts
+++ controllers/accountController.ts
@@ -1,10 +1,15 @@
-import { listAccounts as list} from "../services/sfdcAccounts";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { listAccounts as list } from "../services/sfdcAccounts";
 
-export async function listAccounts(req, res) {
-    const { sessionId, serverUrl } = req.body;
+export default async function listAccounts (req: FastifyRequest, res: FastifyReply) {
 
+    const { sessionId, serverUrl } = req.body as {sessionId: string, serverUrl: string};
     const context = {sessionId, serverUrl};
-    const accounts = await list(context);
 
-    return res.send(accounts);
+    try {
+        const accounts = await list(context);
+        return res.send(accounts);
+    } catch (err) {
+        return res.status(500).send({error: 'Failed to retrieve accounts'});
+    }
 }
