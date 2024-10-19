7550396dc57cdfb43ebd6d03a085aa2f0c3c7396
shreyask0712 <shreyask0712@gmail.com>
2024-10-20 03:49:26 +0530

    sfdc Opportunities: create, update, delete

--- services/sfdcOpportunities.ts
+++ services/sfdcOpportunities.ts
@@ -1,5 +1,30 @@
 import { Context, query } from "./sfdcService";
 
 export async function listOpportunities(context: Context) {
-    return await query(context,'SELECT id, Name, StageName, Amount, AccountId from Opportunities');
+    return await query(context,'SELECT id, Name, StageName, Amount, AccountId from Opportunity');
+}
+
+export async function createOpportunities(context: Context, opportunityData: any) {
+    return await query(context, `INSERT INTO Opportunity (Name, StageName, Amount, AccountId)
+            VALUES (
+                '${opportunityData.Name}',
+                '${opportunityData.StageName}',
+                '${opportunityData.Amount}',
+                '${opportunityData.AccountId}'
+            )
+        `);
+}
+
+export async function updateOpportunities(context: Context, opportunityData: any, opportunityId: string) {
+    return await query(context, `UPDATE Opportunity SET
+            Name = '${opportunityData.Name}',
+            StageName = '${opportunityData.StageName}',
+            Amount = '${opportunityData.Amount}',
+            AccountId = '${opportunityData.accountId}'
+            WHERE Id = '${opportunityId}'
+        `);
+}
+
+export async function deleteOpportunity(context: Context, opportunityId: string) {
+    return await query(context, `DELETE FROM Opportunity WHERE Id = '${opportunityId}'`)
 }
