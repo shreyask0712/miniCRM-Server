f3eaea8bed2ecd1334aef02c1ced17e1ff5ef095
shreyask0712 <shreyask0712@gmail.com>
2024-10-20 02:36:33 +0530

    contact routes - list contacts

--- routes/contacts.ts
+++ routes/contacts.ts
@@ -1,3 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { listContacts } from "../controllers/contactController";
 
+export default async function (fastify: FastifyInstance) {
+    fastify.post('/contacts', listContacts);
+}
